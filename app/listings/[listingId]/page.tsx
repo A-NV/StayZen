df32f977b6e95799498ca4b648c8d7ddc5ce62f5
A-NV <anthony.neav020@gmail.com>
2023-07-10 19:45:39 +1000

    ILVs done + reservations functionality added

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,9 +1,36 @@
-const ListingPage = () => {
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getListingById from "@/app/actions/getListingById";
+import EmptyState from "@/app/components/EmptyState";
+import ClientOnly from "@/app/components/navbar/ClientOnly";
+import ListingClient from "./ListingClient";
+import getReservations from "@/app/actions/getReservations";
+
+interface IParams{
+  listingId?: string;
+}
+
+
+const ListingPage = async ({ params }: { params: IParams}) => {
+  const listing = await getListingById(params);
+  const reservation = await getReservations(params);
+  const currentUser = await getCurrentUser();
+
+  if (!listing) {
+    return (
+      <ClientOnly>
+          <EmptyState />
+      </ClientOnly>
+    )
+  }
   return ( 
-    <div>
-      Listing Page
-    </div>
+    <ClientOnly>
+      <ListingClient
+        listing={listing}
+        reservations={reservation}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
    );
 }
  
-export default ListingPage;
+export default ListingPage; 
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,4 @@
-import { Listing, User } from "@prisma/client";
+import { Listing, Reservation, User } from "@prisma/client";
 
 export type SafeListing = Omit<
   Listing,
@@ -6,6 +6,16 @@ export type SafeListing = Omit<
 > & {
   createdAt: string;
 }
+
+export type SafeReservation = Omit<
+  Reservation,
+  "createdAt" | "startDate" | "endDate" | "listing"
+  > & {
+    createdAt: string,
+    startDate: string,
+    endDate: string,
+    listing: SafeListing 
+  }
 
 export type SafeUser = Omit<
   User,
