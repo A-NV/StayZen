44ca88f92cd1da6ce8668b2300087d55faea3a48
A-NV <anthony.neav020@gmail.com>
2023-07-11 22:32:18 +1000

    final v2

--- app/actions/getListingById.ts
+++ app/actions/getListingById.ts
@@ -1,40 +1,95 @@
 import prisma from "@/app/libs/prismadb";
-import { listenerCount } from "process";
 
-interface IParams {
-  listingId?: string;
+export interface IListingsParams {
+  userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
-export default async function getListingById(
-  params: IParams
+export default async function getListings(
+  params: IListingsParams
 ) {
   try {
-    const {listingId} = params;
+    const {
+      userId,
+      roomCount, 
+      guestCount, 
+      bathroomCount, 
+      locationValue,
+      startDate,
+      endDate,
+      category,
+    } = params;
 
-    const listing = await prisma.listing.findUnique({
-      where: {
-        id: listingId
-      },
-      include: {
-        user: true
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId;
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount
+      }
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount
+      }
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount
+      }
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate }
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate }
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    const listings = await prisma.listing.findMany({
+      where: query,
+      orderBy: {
+        createdAt: 'desc'
       }
     });
 
-    if (!listing) {
-      return null;
-    }
-
-    return {
+    const safeListings = listings.map((listing) => ({
       ...listing,
       createdAt: listing.createdAt.toISOString(),
-      user: {
-        ...listing.user,
-        createdAt: listing.user.createdAt.toISOString(),
-        updatedAt: listing.user.updatedAt.toISOString(),
-        emailVerified: 
-          listing.user.emailVerified?.toString() || null,
-      }
-    };
+    }));
+
+    return safeListings;
   } catch (error: any) {
     throw new Error(error);
   }
